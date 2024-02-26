f53e2d2b4402a00c4222e960cff4598e5d324353
joeraymon <joe.raymon10@gmail.com>
2024-02-26 07:11:26 -0600

    add test route

--- api/index.py
+++ api/index.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'hello world!'
+
+@app.route('/test')
+def test():
+    print(request.json())
+
+    return jsonify({"message": "Data received"}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
